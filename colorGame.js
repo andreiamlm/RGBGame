4ef98122af988b6d53c435976aca35e2baf60890
andreiamlm <andreiamlm@gmail.com>
2017-03-24 11:35:16 +0000

    added play again button

--- colorGame.js
+++ colorGame.js
@@ -4,6 +4,18 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay= document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+ colors = generateRandomColors(6);
+ pickedColor= pickColor();
+ colorDisplay.textContent = pickedColor;
+ for(var i = 0; i < squares.length; i++) {
+ 	squares[i].style.background = colors[i];
+ }
+ h1.style.background = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -13,7 +25,9 @@ for(var i=0; i < squares.length; i++) {
 		var clickedColor = this.style.background;
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?"
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 		this.style.background= "#232323";
 		messageDisplay.textContent="Try Again"
